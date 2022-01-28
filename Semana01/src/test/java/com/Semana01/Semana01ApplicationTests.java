21b837eb0342e3de94a274bcf718e6acd670bfb9
Gabrielmong <84334533+Gabrielmong@users.noreply.github.com>
2022-01-28 11:59:05 -0600

    Initial Commit
    Main Project Files

---
+++ Semana01/src/test/java/com/Semana01/Semana01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Semana01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
